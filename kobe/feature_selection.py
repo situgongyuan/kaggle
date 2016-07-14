184d74bd2c8b3845529480aecc02efa9034473e4
stgy <situgongyuan@gmail.com>
2016-07-14 20:04:53 +0800

    scikit-learn testing

---
+++ kobe/feature_selection.py
@@ -0,0 +1,43 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.cross_validation import KFold, cross_val_score
+from sklearn.metrics import make_scorer
+from sklearn.grid_search import GridSearchCV
+from sklearn.feature_selection import VarianceThreshold, RFE, SelectKBest, chi2
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
+
+sns.set_style('whitegrid')
+pd.set_option('display.max_columns', None) # display all columns
+
+data = pd.read_csv('data.csv')
+
+data.set_index('shot_id', inplace=True)
+data["action_type"] = data["action_type"].astype('object')
+data["combined_shot_type"] = data["combined_shot_type"].astype('category')
+data["game_event_id"] = data["game_event_id"].astype('category')
+data["game_id"] = data["game_id"].astype('category')
+data["period"] = data["period"].astype('object')
+data["playoffs"] = data["playoffs"].astype('category')
+data["season"] = data["season"].astype('category')
+data["shot_made_flag"] = data["shot_made_flag"].astype('category')
+data["shot_type"] = data["shot_type"].astype('category')
+data["team_id"] = data["team_id"].astype('category')
+
+print sns.__version__
+
+
